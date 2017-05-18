13807455ff10a214ade605eb9de1fb94e3d5e218
Ken Courville <kcourville@bluedotsolutions.com>
2017-05-17 18:29:35 -0600

    add helper for retrieving message Q

---
+++ src/GettingStartedApplication/StatefulBackendService/Extensions/IReliableStateManagerExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.ServiceFabric.Data;
+using Microsoft.ServiceFabric.Data.Collections;
+using StatelessBackendService.Interfaces.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StatefulBackendService
+{
+    public static class IReliableStateManagerExtensions
+    {
+
+        public async static Task<IReliableQueue<Message>> GetMessageQueue(this IReliableStateManager stateManager)
+        {
+            return await stateManager.GetOrAddAsync<IReliableQueue<Message>>(StatefulBackendService.messageQueue);
+        }
+    }
+}
